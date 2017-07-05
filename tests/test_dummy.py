d9e3a3b1b3e97788e1bfaccf2550da9fd810e170
Bolaji Olajide <bolaji.olajide@andela.com>
2017-07-05 15:19:05 +0100

    chore(tests): change naming convention for test files

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -8,7 +8,3 @@ class DummyTest(unittest.TestCase):
     def testDummyFunction(self):
         result = multiplyDigits(2, 3)
         self.assertEqual(result, 6)
-
-
-if __name__ == "__main__":
-    unittest.main()
