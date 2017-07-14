2c9c19545c723d76ce0c55edead8740aacbfe9fa
dennomwas <githinji.mwangi@gmail.com>
2017-07-14 17:14:55 +0300

    ft(contributions) Get specific user's contribution

--- api/test/test_main.py
+++ api/test/test_main.py
@@ -1,11 +1,7 @@
-from learning_map_api.api import create_flask_app
 from learning_map_api.api.test.base import BaseTestCase
-# import unittest
 
 
 class TestMain(BaseTestCase):
     def test_app_get(self):
-        self.app = self.app.test_client()
-        response = self.app.get('/')
-        # this should be changed to assert200, it's a bug
-        self.assert404(response)
+        response = self.client.get('/')
+        self.assert200(response)
